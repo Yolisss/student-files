c252f2aaac09d28217fee1d32f4ce15bed3d7614
Yolis <yolisisabel28@gmail.com>
2023-11-20 12:38:14 -0800

    initial set up and rendered 12 students on console

---
+++ js/script.js
@@ -0,0 +1,6 @@
+fetch('https://randomuser.me/api/?results=12')
+//reads the response and returns a promise that resolves to json
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+
